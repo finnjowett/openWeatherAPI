429296ca0ed66c11d4c6023a5d40eeb594f0ab00
finn <finnjowett971108@gmail.com>
2022-11-17 14:35:23 -0800

    done

--- weatherAPI/app.js
+++ weatherAPI/app.js
@@ -1,25 +1,39 @@
-// api :endpoint(url), path, query
-// fetch api needs to use json() method
-// async function getJoke() {
-//   let joke = await fetch("https://v2.jokeapi.dev/joke/Christmas");
-//   let data = await joke.json();
-//   console.log(data.setup);
-// }
-// getJoke();
+const fetch = require("node-fetch");
+const express = require("express");
+app = express();
+const ejs = require("ejs");
 
-// const btn = document.querySelector(".btn");
-// btn.addEventListener("click", add);
-// function add() {
-//   const first = document.querySelector("#first");
-//   const second = document.querySelector("#second");
-//   alert(Number(first.value) + Number(second.value));
-// }
 let myKey = "f014be697d15299e50a89ebadd2b8c39";
-let city = "Paris";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
-async function getWeather() {
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/:city", async (req, res) => {
+  // deconstructing an object
+  let { city } = req.params;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
   let d = await fetch(url);
   let dj = await d.json();
+  let { temp } = dj.main;
+  let newT = kToC(temp);
   console.log(dj);
+  res.render("weather.ejs", { dj, city, newT });
+});
+
+app.listen(3000);
+
+// let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
+// async function getWeather() {
+//   let d = await fetch(url);
+//   let dj = await d.json();
+//   console.log(dj);
+// }
+// getWeather();
+
+function kToC(k) {
+  return Math.round(k - 273.15);
 }
-getWeather();
